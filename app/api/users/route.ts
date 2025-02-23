469c0aac01b3350c3ea71da637da81af8e760c4c
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-23 22:39:13 +0000

    Create user

---
+++ app/api/users/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    const { email, password, confirmPassword } = await req.json();
+
+    if (!email || !password || !confirmPassword) {
+      return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+    }
+
+    if (password !== confirmPassword) {
+      return NextResponse.json({ message: "Passwords do not match" }, { status: 400 });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
